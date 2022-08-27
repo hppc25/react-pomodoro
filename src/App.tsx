c5fdcdc834f276c5ddaa1a61bae52e61ad36e8b8
Helder Correia <helder.patrick25@gmail.com>
2022-08-27 12:06:54 +0100

    Add and config style componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <div className="App">
-      <p>Hello react</p>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
